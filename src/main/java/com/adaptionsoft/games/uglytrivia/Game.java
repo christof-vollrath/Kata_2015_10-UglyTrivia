88dd3e47dcfb159b3af45c598c362d0290c6365b
Christof Vollrath <christof@taobits.net>
2015-10-26 21:42:05 +0100

    Changed typo from corrent to correct

--- src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
+++ src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -167,7 +167,7 @@ public class Game {
 				return false;
 			}
 		} else {
-			System.out.println("Answer was corrent!!!!");
+			System.out.println("Answer was correct!!!!");
 			purses[currentPlayer]++;
 			System.out.println(players.get(currentPlayer) 
 					+ " now has "
